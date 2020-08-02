a78f4facfca2c0551668b700cc5496ea0d6f1581
takara1356 <goblin7283@gmail.com>
2020-08-02 18:12:16 +0900

    appBar,Centerの配置

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('はろーわーるど'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://i.pinimg.com/originals/a8/02/aa/a802aaa755fd5a4ef292813adcdb5094.jpg'),
+          ),
+        ),
       ),
     ),
   );
