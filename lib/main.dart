f3d51e66266309fd1c7c44273a24fa8b38a4c091
takara1356 <goblin7283@gmail.com>
2020-08-04 21:38:55 +0900

    アイコンの追加

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,8 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://i.pinimg.com/originals/a8/02/aa/a802aaa755fd5a4ef292813adcdb5094.jpg'),
+            image: AssetImage(
+                'images/diamond.png'),
           ),
         ),
       ),
